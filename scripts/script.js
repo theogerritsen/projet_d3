1aecfd1ee9e2a9100bd55a815816382af30bed2e
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2021-04-22 11:09:28 +0200

    Changement du fond de carte en SVG
    Modification du fond de carte pour utiliser du d3 et du SVG. Problème de version en d3 : n'arrive pas à afficher la carte avec la version actuelle de d3. Aller voir les modifications.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,32 @@
-var Path = 'data/europe.geo.json';
-        var MapStyle = {
-            stroke: false,
-            fill: true,
-            fillColor: '#fff',
-            fillOpacity: 1
-        }
-        $.getJSON(Path,function(data){
-            var myMap = L.map('map').setView([57, 12], 4);
+// définition de notre canevas SVG
+const width = 800;
+const height = 800;
+// définition de la projection
+// https://github.com/d3/d3-geo#azimuthal-projections
+const projection = d3.geo.mercator()
+    // on  centre la carte par rapport à des coordonnées
+    .center([11, 55])
+    // on définit l'échelle de notre carte
+    .scale(width*5)
+    .translate([width / 2, height / 2]);
+// définition du chemin pour nos données
+const path = d3.geo.path()
+// on peut maintenant assigner notre projection à notre Path
+    .projection(projection);
 
-            L.geoJson(data, {
-                clickable: true,
-                style: MapStyle
-            }).addTo(myMap);
-        })
+// d3.select nous permet d'aller récupérer notre DIV dans le document HTML
+const svg = d3.select("#map")
+    .append("svg")
+    // puis on attribue au DIV les dimensions définies plus haut
+    .attr("width", width)
+    .attr("height", height);
+
+// on charge maintenant le fichier geoJSON (obtenu sur https://geojson-maps.ash.ms/)
+d3.json('data/europe.geo.json', function(geojson){
+    svg.selectAll("path")
+        .data(geojson.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+});
+console.log("hello");
