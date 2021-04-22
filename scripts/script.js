63b264a701c261ec267885cad5029361143de03b
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2021-04-22 11:51:42 +0200

    Changement en d3v6

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,30 @@
 // définition de notre canevas SVG
-const width = 800;
-const height = 800;
+const width = 800, height = 800;
 // définition de la projection
 // https://github.com/d3/d3-geo#azimuthal-projections
-const projection = d3.geo.mercator()
+const projection = d3.geoMercator()
     // on  centre la carte par rapport à des coordonnées
-    .center([11, 55])
+    .center([11, 57])
     // on définit l'échelle de notre carte
-    .scale(width*5)
+    .scale(width/1.25)
     .translate([width / 2, height / 2]);
-// définition du chemin pour nos données
-const path = d3.geo.path()
-// on peut maintenant assigner notre projection à notre Path
-    .projection(projection);
 
 // d3.select nous permet d'aller récupérer notre DIV dans le document HTML
 const svg = d3.select("#map")
     .append("svg")
     // puis on attribue au DIV les dimensions définies plus haut
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
 
+    // définition du chemin pour nos données
+const path = d3.geoPath()
+// on peut maintenant assigner notre projection à notre Path
+    .projection(projection);
+
+const g = svg.append("g");
 // on charge maintenant le fichier geoJSON (obtenu sur https://geojson-maps.ash.ms/)
-d3.json('data/europe.geo.json', function(geojson){
-    svg.selectAll("path")
+d3.json('data/europe.geo.json').then(function(geojson){
+    g.selectAll("path")
         .data(geojson.features)
         .enter()
         .append("path")
